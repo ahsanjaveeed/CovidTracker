40d03c572eb1428133fd24b6d9cf59bfafb79caf
ahsanjaveed <108805508+ahsanjaveed@users.noreply.github.com>
2022-07-16 12:53:37 -0700

    Project Structure - Component & Api

--- covidtracker/src/App.js
+++ covidtracker/src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import styles from './App.module.css';
+import {Charts, Cards, CountryPicker} from './Components'
+import { fetchData } from './api'
+import React from 'react';
 
-function App() {
+class App extends React.Component {
+
+  async componentDidMount(){
+    const data = await fetchData();
+    console.log(data)
+  }
+
+render(){
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={styles.container}>
+      <Cards />
+      <CountryPicker />
+      <Charts />
     </div>
   );
+}
+
 }
 
 export default App;
